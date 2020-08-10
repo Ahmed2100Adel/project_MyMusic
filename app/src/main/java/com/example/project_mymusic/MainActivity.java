b8139ccefe6906401e1fc64b79b6fa942bc9ea48
aaahhhmmmeeeddd <AH644Hu@ya>
2020-08-10 06:52:10 +0200

    adding periodic change of background

--- app/src/main/java/com/example/project_mymusic/MainActivity.java
+++ app/src/main/java/com/example/project_mymusic/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.project_mymusic;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.example.project_mymusic.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,12 +23,22 @@ import android.view.View;
 
 import com.example.project_mymusic.ui.main.SectionsPagerAdapter;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View root=binding.getRoot();
+        setContentView(root);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
@@ -35,5 +53,36 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        settingBackgroundPeriodically();
+    }
+
+    public void settingBackgroundPeriodically(){
+        Observable.interval(0,20, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(o->{
+                    Random random= new Random();
+                    int max=21;
+                    int min=1;
+                    int ran=random.nextInt((max-min)+1)+min;
+                    String uri="@drawable/background_"+ran;
+                    int imageRes=getResources().getIdentifier(uri,null,getPackageName());
+                    Glide.with(this)
+                            .load(imageRes)
+                            .transition(DrawableTransitionOptions.withCrossFade(1000))
+                            .into(new CustomTarget<Drawable>() {
+                                @Override
+                                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                                    binding.viewPager.setBackground(resource);
+                                }
+
+                                @Override
+                                public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                }
+                            });
+                });
+
+
     }
 }
